842316cfec5f7bee1e102c19e09ae9bf510f246c
Javohirbek <safarovjavohirbek3@gmail.com>
2026-04-21 14:06:33 +0500

    Add liquid_pull_to_refresh package and implement pull-to-refresh functionality in TodosPage

--- smart_todos_flutter/lib/screens/todos_page.dart
+++ smart_todos_flutter/lib/screens/todos_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 import 'package:serverpod_auth_idp_flutter/serverpod_auth_idp_flutter.dart';
 import 'package:smart_todos_client/smart_todos_client.dart';
 import 'package:smart_todos_flutter/main.dart';
@@ -20,13 +21,13 @@ class _TodosPageState extends State<TodosPage> {
     getTodos();
   }
 
-  void getTodos() {
+  Future<void> getTodos() async {
     if (mounted) {
-      client.todo.getTodos().then((value) {
-        setState(() {
-          _todos.clear();
-          _todos.addAll(value);
-        });
+      final value = await client.todo.getTodos();
+      setState(() {
+        _todos.clear();
+        _todos.addAll(value);
+        return;
       });
     }
   }
@@ -138,8 +139,10 @@ class _TodosPageState extends State<TodosPage> {
 
   @override
   Widget build(BuildContext context) {
+    int _screenHeight = MediaQuery.of(context).size.height.toInt();
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
         title: const Text('My Todos'),
         actions: [
           IconButton(
@@ -170,70 +173,90 @@ class _TodosPageState extends State<TodosPage> {
           ),
         ],
       ),
-      body: _todos.isNotEmpty
-          /// if there are todos, show them in a ListView
-          ? ListView.separated(
-              itemCount: _todos.length,
-              separatorBuilder: (context, index) =>
-                  Container(height: 1, color: Colors.grey[300]),
-              itemBuilder: (context, index) {
-                var todo = _todos[index];
-                return ListTile(
-                  leading: Checkbox(
-                    value: todo.isCompleted,
-                    onChanged: (value) => changeChecked(todo, index),
+      body: LiquidPullToRefresh(
+        showChildOpacityTransition: false,
+        animSpeedFactor: 5,
+        height: 50,
+        onRefresh: getTodos,
+        color: Theme.of(context).colorScheme.primaryContainer,
+        child: _todos.isNotEmpty
+            /// if there are todos, show them in a ListView
+            ? ListView.separated(
+                itemCount: _todos.length,
+                separatorBuilder: (context, index) =>
+                    Container(height: 1, color: Colors.grey[300]),
+                itemBuilder: (context, index) {
+                  var todo = _todos[index];
+                  return ListTile(
+                    leading: Checkbox(
+                      value: todo.isCompleted,
+                      onChanged: (value) => changeChecked(todo, index),
+                    ),
+                    title: Text(
+                      todo.title,
+                      style: TextStyle(color: Colors.black, fontSize: 15),
+                    ),
+                    subtitle: Text(
+                      todo.description ?? '',
+                      style: TextStyle(color: Colors.black54, fontSize: 11),
+                    ),
+                    trailing: Text(
+                      todo.createdAt.compareTo(
+                                DateTime.now().subtract(
+                                  const Duration(days: 1),
+                                ),
+                              ) >
+                              0
+                          ? todo.createdAt.toLocal().toString().substring(
+                              11,
+                              16,
+                            )
+                          : "${todo.createdAt.toLocal().toString().substring(11, 16)}\n"
+                                "${todo.createdAt.toLocal().toString().substring(0, 10)}",
+                      textAlign: TextAlign.end,
+                    ),
+                    onTap: () => changeChecked(todo, index),
+                    onLongPress: () => client.todo
+                        .deleteTodo(todo.id!)
+                        .then((_) {
+                          if (mounted) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text('Todo deleted: ${todo.title}'),
+                              ),
+                            );
+                            getTodos();
+                          }
+                        })
+                        .catchError((error) {
+                          if (mounted) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text('Error deleting todo: $error'),
+                              ),
+                            );
+                          }
+                        }),
+                  );
+                },
+              )
+            /// if there are no todos, show a friendly message
+            : ListView(
+                children: [
+                  SizedBox(height: _screenHeight / 2 - 100),
+                  Text(
+                    'No todos yet',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontStyle: FontStyle.italic),
                   ),
-                  title: Text(
-                    todo.title,
-                    style: TextStyle(color: Colors.black, fontSize: 15),
+                  Text(
+                    'Tap + to add your first todo!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontStyle: FontStyle.italic),
                   ),
-                  subtitle: Text(
-                    todo.description ?? '',
-                    style: TextStyle(color: Colors.black38, fontSize: 11),
-                  ),
-                  trailing: Text(
-                    todo.createdAt.compareTo(
-                              DateTime.now().subtract(const Duration(days: 1)),
-                            ) >
-                            0
-                        ? todo.createdAt.toLocal().toString().substring(11, 16)
-                        : "${todo.createdAt.toLocal().toString().substring(11, 16)}\n"
-                              "${todo.createdAt.toLocal().toString().substring(0, 10)}",
-                    textAlign: TextAlign.end,
-                  ),
-                  onTap: () => changeChecked(todo, index),
-                  onLongPress: () => client.todo
-                      .deleteTodo(todo.id!)
-                      .then((_) {
-                        if (mounted) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text('Todo deleted: ${todo.title}'),
-                            ),
-                          );
-                          getTodos();
-                        }
-                      })
-                      .catchError((error) {
-                        if (mounted) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text('Error deleting todo: $error'),
-                            ),
-                          );
-                        }
-                      }),
-                );
-              },
-            )
-          /// if there are no todos, show a friendly message
-          : const Center(
-              child: Text(
-                'No todos yet\nTap + to add your first todo!',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontStyle: FontStyle.italic),
+                ],
               ),
-            ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => showAddTodoDialog(),
         child: const Icon(Icons.add),
